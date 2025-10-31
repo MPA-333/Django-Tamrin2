ffc11dee6ddbafe137bec82464a61f96165d03e0
MPA-333 <peymanaghdam@yahoo.com>
2025-11-01 00:33:58 +0330

    Create Department Page
    view mored nazar gablan sakhte shode bud.
    template mored nazar tarrahi shod.
    template haye vabaste eslah shod.

--- app/introduction/urls.py
+++ app/introduction/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app.introduction.views import introView, historyView
+from app.introduction.views import introView, historyView, departmentView
 
 urlpatterns = [
     path("", introView, name="intro"),
     path("history/", historyView, name="history"),
+    path("department/", departmentView, name="department")
 ]
