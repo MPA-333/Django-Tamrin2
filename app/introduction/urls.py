bb9c8f1619d935267f996ba447765632c7443423
MPA-333 <peymanaghdam@yahoo.com>
2025-10-31 17:51:36 +0330

    Create History Page
    safhe history dar app Introduction sakhte shod.
    url, view marbute sakhte shod.
    file html mabute tarrahi shod.

--- app/introduction/urls.py
+++ app/introduction/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.introduction.views import introView
+from app.introduction.views import introView, historyView
 
 urlpatterns = [
     path("", introView, name="intro"),
+    path("history/", historyView, name="history"),
 ]
--- app/introduction/views.py
+++ app/introduction/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def introView(request):
     return render(request, "introduction/intro.html")
+
+
+def historyView(request):
+    return render(request, "introduction/history.html")
